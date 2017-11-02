1acb534ef0db52c2fcd353871dcec6ae53a58ca5
Dima Gimburg <efim2004@gmail.com>
2017-11-02 15:30:39 +0200

    added csv support

--- multithread_worker.py
+++ multithread_worker.py
@@ -1,14 +1,24 @@
+import csv
 from threading import Thread
 from Queue import Queue
 
 
 class LineByLineJob:
-    NUM_THREADS = 20
-    input_queue = Queue()
+    NUM_THREADS = 100
+    input_queue = Queue(100)
 
-    def __init__(self, input_file_path, single_line_worker, debug=False):
-        self.single_line_worker = single_line_worker
+    def __init__(self, input_file_path, single_line_worker, output_file_path=None, is_csv=False, debug=False):
+        """
+        :param input_file_path: input file with lines to process
+        :param single_line_worker: a reference to a function that processes a single line from the input file ('''def worker(single_line):''')
+        :param output_file_path: if set to non
+        :param is_csv: if is_csv set to true each line returned as array of csv line split by ,
+        :param debug: verbose logging
+        """
         self.input_file_path = input_file_path
+        self.output_file_path = output_file_path
+        self.single_line_worker = single_line_worker
+        self.is_csv = is_csv
         self.debug = debug
         self.finished = False
 
@@ -25,14 +35,20 @@ class LineByLineJob:
 
     def _put_input_into_queue(self):
         with open(self.input_file_path, 'rb') as input_file:
+            if self.is_csv:
+                input_file = csv.reader(input_file)
             for index, line in enumerate(input_file):
-                line = line.strip()
-                self.input_queue.put(line + "\n")
+                if self.is_csv:
+                    self.input_queue.put(line)
+                else:
+                    line = line.strip()
+                    self.input_queue.put(line + "\n")
+
                 if self.debug:
                     print(
                     "[DEUBG ::Line added to queue] - [line_number={line_number}] [line={line_text}]".format(**{
                         'line_number': index,
-                        'line_text': line
+                        'line_text': str(line)
                     }))
 
             # block until all tasks done
@@ -57,7 +73,8 @@ class LineByLineJobThread(Thread):
             # time.sleep(1)
             input_line = self.input_queue.get()
             proccessed = self.line_worker(input_line)
-            self.append_to_thread_file(proccessed)
+            if proccessed:
+                self.append_to_thread_file(proccessed)
             self.input_queue.task_done()
         return
 
